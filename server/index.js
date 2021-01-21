166b1adfa9d2218de17aeeef82a05cdeee3d703d
SASIKANTBISWAL <biswalsashikant@gmail.com>
2021-01-22 02:08:37 +0530

    Added welcome route in serer

--- server/index.js
+++ server/index.js
@@ -16,7 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://node:node123@cluster0.notzu.mongodb.net/memories?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
