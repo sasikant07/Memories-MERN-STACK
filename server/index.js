d2105910657a6175ddebee0eeb9bada3de21383e
SASIKANTBISWAL <biswalsashikant@gmail.com>
2021-01-22 01:46:11 +0530

    complete feature of CRUD

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -14,10 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://node:node123@cluster0.notzu.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://node:node123@cluster0.notzu.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
